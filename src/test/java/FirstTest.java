3102c47fb089bcd995efc2acc580faa1451fb48e
SachokA <agukavchyk+10@gmail.com>
2024-04-02 15:49:25 +0300

    second commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -52,6 +52,7 @@ public class FirstTest extends BaseTest {
         System.out.println(homePage.isAltValue());
         Assert.assertTrue(homePage.isImagContainsAltValue());
     }
+
     @Feature("Checks that have be done on all pages")
     @Severity(SeverityLevel.NORMAL)
     @Description("Test to verify isn't Loren Ipson")
@@ -67,7 +68,7 @@ public class FirstTest extends BaseTest {
         ContactUsPage contactUsPage = new ContactUsPage();
         footerPage.clickElementContact();
 
-        Assert.assertEquals(contactUsPage.getTitleContactUs(),"vhvhj");
+        Assert.assertEquals(contactUsPage.getTitleContactUs(), "vhvhj");
     }
 
     @Feature("Search page")
@@ -76,35 +77,14 @@ public class FirstTest extends BaseTest {
     public void isPresentSearchPageTest() {
         HomePage homePage = new HomePage();
         homePage.isPresentSearchPage();
-        Assert.assertTrue(homePage.isPresentSearchPage(),"Input search is not enabled on this page");
+        Assert.assertTrue(homePage.isPresentSearchPage(), "Input search is not enabled on this page");
     }
+
     @Test
-    public void checkShouldNotHaveHorizontalScrollbar () {
+    public void checkShouldNotHaveHorizontalScrollbar() {
         HomePage homePage = new HomePage();
         Assert.assertFalse(homePage.isHorizontalScrollBarPresent());
     }
-
-//    @Test
-//    public void isEmptySearchTerm() {
-//        HomePage homePage = new HomePage();
-//        homePage.openSearchPage();
-//        homePage.setInputSearch("hjhb");
-////        Assert.assertEquals();
-//    }
-//
-//    @Test
-//    public void testHeadingOrder() {
-//        List<WebElement> headers = driver.findElements(By.xpath("//h1|//h2|//h3|//h4|//h5|//h6"));
-//
-//        for (int i = 1; i < headers.size(); i++) {
-//            WebElement previousHeader = headers.get(i - 1);
-//            WebElement currentHeader = headers.get(i);
-//
-//            int previousLevel = Integer.parseInt(previousHeader.getTagName().substring(1));
-//            int currentLevel = Integer.parseInt(currentHeader.getTagName().substring(1));
-//
-//            Assert.assertTrue(currentLevel > previousLevel, "Headings are not in sequential order");
-//        }
-    }
+}
 
 
